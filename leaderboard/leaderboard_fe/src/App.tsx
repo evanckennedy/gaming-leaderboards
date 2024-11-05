7f1e35f0c1e499f5185c8ead9755d9e25c528ce3
Matthew Banuag <m.banuag@bsdxr.com>
2024-11-05 14:20:26 -0600

    Update App.tsx with new text to test husky and prettier

--- leaderboard/leaderboard_fe/src/App.tsx
+++ leaderboard/leaderboard_fe/src/App.tsx
@@ -1,9 +1,5 @@
 const App = () => {
-  return (
-    <div className="text-3xl font-bold">
-      Hello world
-    </div>
-  )
-}
+  return <div className="text-3xl font-bold">Hello world I am testing</div>;
+};
 
-export default App
+export default App;
