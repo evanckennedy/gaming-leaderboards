1a9144a403f0a3a35c634b1a4a63f0c2fffc2d5d
Matthew Banuag <m.banuag@bsdxr.com>
2024-11-06 07:45:41 -0600

    Update index.ts with removal of husky and prettier test object

--- leaderboard/leaderboard_be/index.ts
+++ leaderboard/leaderboard_be/index.ts
@@ -1,10 +1,6 @@
 import express from "express";
 const app = express();
 const PORT = 8000;
-const object = {
-  test: "test",
-  test2: "test",
-};
 
 app.get("/", (req, res) => {
   res.send("Hello world");
