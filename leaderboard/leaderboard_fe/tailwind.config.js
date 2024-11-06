55bbb5cd2ec15a67649ed3a289f5dfa6be9386a4
Evan Kennedy <evanckennedy@protonmail.com>
2024-11-06 13:20:16 -0600

    Update tailwind.config with color palette

--- leaderboard/leaderboard_fe/tailwind.config.js
+++ leaderboard/leaderboard_fe/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deepPurple: "#340068",
+        veryDarkPurple: "#1A0034",
+        white: "#FFFFFF",
+        mediumGray: "#707070",
+        brightOrange: "#FF6F00",
+        lavenderGray: "#8782B3",
+        vibrantPurple: "#4D0099",
+        paleGreen: "#F2F5EA",
+        mutedPurple: "#726E97",
+      },
+    },
   },
   plugins: [],
-}
-
+};
