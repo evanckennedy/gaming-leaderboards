2b108cfb9d03ff06eb7a89405d49e25916d4919e
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-10 15:46:40 -0600

    Update ProtectedRoute.tsx with doc block explaining the component logic

--- leaderboard/leaderboard_fe/src/components/protectedRoutes/ProtectedRoute.tsx
+++ leaderboard/leaderboard_fe/src/components/protectedRoutes/ProtectedRoute.tsx
@@ -7,6 +7,16 @@ interface ProtectedRouteProps {
   element: React.ReactElement;
 }
 
+/**
+ * A component that protects routes based on user authentication and authorization.
+ *
+ * This component performs the following checks:
+ * - If the user is not logged in, it redirects to the sign-in page.
+ * - If the user is logged in but does not have the required role, it redirects
+ *   to the "Not Authorized" page.
+ * - If the user is logged in and has the required role, it renders the
+ *   specified element.
+ */
 function ProtectedRoute({ allowedRoles, element }: ProtectedRouteProps) {
   const { isLoggedIn, roleName } = useSelector(
     (state: RootState) => state.user,
