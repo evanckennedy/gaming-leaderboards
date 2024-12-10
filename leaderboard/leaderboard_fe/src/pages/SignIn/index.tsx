d4ee36235815dc226a5c010eebc4cfa7849c22ac
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-10 16:02:15 -0600

    Redirect user to home if user is already logged in on SignIn page

--- leaderboard/leaderboard_fe/src/pages/SignIn/index.tsx
+++ leaderboard/leaderboard_fe/src/pages/SignIn/index.tsx
@@ -3,9 +3,21 @@ import SignInForm from "@/features/authentication/components/SignInForm";
 import SignUpForm from "@/features/authentication/components/SignUpForm";
 import ToggleAuthenticationForm from "@/features/authentication/components/ToggleAuthenticationForm";
 import useAuthenticationState from "./hooks/useAuthenticationState";
+import { useEffect } from "react";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
   const { isSignUp, toggleForm } = useAuthenticationState();
+  const { isLoggedIn } = useSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/");
+    }
+  }, [isLoggedIn, navigate]);
 
   return (
     <>
