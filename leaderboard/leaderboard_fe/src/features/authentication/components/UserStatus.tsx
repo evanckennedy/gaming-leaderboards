59e23e8daac2d4d923af080e9e5ebee48c409bcc
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-11 15:55:19 -0600

    Create UserStatus component to display user information and logout button

---
+++ leaderboard/leaderboard_fe/src/features/authentication/components/UserStatus.tsx
@@ -0,0 +1,36 @@
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/store/store";
+import { logout } from "@/features/authentication/slices/userSlice";
+
+function UserStatus() {
+  const dispatch = useDispatch();
+  const { isLoggedIn, userId, firstName, lastName } = useSelector(
+    (state: RootState) => state.user,
+  );
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  if (!isLoggedIn) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col">
+      <p className="text-white-100 text-xl 3xl:text-3xl 4xl:text-6xl">
+        <span>Signed in as: </span>
+        <span>{firstName} </span>
+        <span>{lastName}</span>
+      </p>
+      <button
+        onClick={handleLogout}
+        className="text-white-100 text-xl 3xl:text-3xl 4xl:text-6xl hover:text-secondary transition-colors duration-300 ease-out"
+      >
+        Sign Out &gt;&gt;
+      </button>
+    </div>
+  );
+}
+
+export default UserStatus;
