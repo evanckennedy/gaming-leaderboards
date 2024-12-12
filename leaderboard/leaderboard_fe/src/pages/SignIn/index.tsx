f9c5e55e29d247d8bda355aef05acb457c8fa97c
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-12 10:56:09 -0600

    Update sign in page with updated conditional rendering to include the reset password form

--- leaderboard/leaderboard_fe/src/pages/SignIn/index.tsx
+++ leaderboard/leaderboard_fe/src/pages/SignIn/index.tsx
@@ -1,17 +1,31 @@
 import PageTitle from "@/components/ui/pageTitle/PageTitle";
 import SignInForm from "@/features/authentication/components/SignInForm";
 import SignUpForm from "@/features/authentication/components/SignUpForm";
+import ResetPasswordForm from "@/features/authentication/components/ResetPasswordForm";
 import ToggleAuthenticationForm from "@/features/authentication/components/ToggleAuthenticationForm";
 import useAuthenticationState from "./hooks/useAuthenticationState";
 import { useEffect } from "react";
 import { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function SignIn() {
   const { isSignUp, toggleForm } = useAuthenticationState();
   const { isLoggedIn } = useSelector((state: RootState) => state.user);
   const navigate = useNavigate();
+  const [showResetPassword, setShowResetPassword] = useState(false);
+
+  const toggleResetPassword = () => {
+    setShowResetPassword(!showResetPassword);
+  };
+
+  let title = "Sign In";
+  if (isSignUp) {
+    title = "Sign Up";
+  } else if (showResetPassword) {
+    title = "Reset Password";
+  }
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -21,11 +35,15 @@ function SignIn() {
 
   return (
     <>
-      <PageTitle title={isSignUp ? "Sign Up" : "Sign In"} />
+      <PageTitle title={title} />
       {isSignUp ?
         <SignUpForm />
-      : <SignInForm />}
-      <ToggleAuthenticationForm isSignUp={isSignUp} toggleForm={toggleForm} />
+      : showResetPassword ?
+        <ResetPasswordForm onBack={toggleResetPassword} />
+      : <SignInForm onToggleResetPassword={toggleResetPassword} />}
+      {!showResetPassword && (
+        <ToggleAuthenticationForm isSignUp={isSignUp} toggleForm={toggleForm} />
+      )}
     </>
   );
 }
