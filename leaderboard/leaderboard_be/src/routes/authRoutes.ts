6d234284956774bbd3de43373bb16fc37564ba94
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-13 10:51:54 -0600

    Update authRoutes.ts with reset password route commented out

--- leaderboard/leaderboard_be/src/routes/authRoutes.ts
+++ leaderboard/leaderboard_be/src/routes/authRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { signUp, signIn, resetPassword } from "../controllers/authController";
+import { signUp, signIn } from "../controllers/authController";
 
 const router = Router();
 
 router.post("/users/signup", signUp);
 router.post("/users/signin", signIn);
-router.patch("/users/password", resetPassword);
+
+// Could be used in the future if you make it more secure
+// router.patch("/users/password", resetPassword);
 
 export default router;
