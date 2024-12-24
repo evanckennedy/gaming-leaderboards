379f00744993e10921661b82b33b25cf1bc6144d
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-24 13:42:08 -0600

    Update leaderboardSorting.ts with other sorting functions

--- leaderboard/leaderboard_fe/src/features/leaderboards/helpers/leaderboardSorting.ts
+++ leaderboard/leaderboard_fe/src/features/leaderboards/helpers/leaderboardSorting.ts
@@ -14,3 +14,20 @@ export function sortByLatest(leaderboards: FormattedLeaderboardData[]) {
     return b.rawGameDate.getTime() - a.rawGameDate.getTime();
   });
 }
+export function sortByOldest(leaderboards: FormattedLeaderboardData[]) {
+  return leaderboards.sort((a, b) => {
+    return a.rawGameDate.getTime() - b.rawGameDate.getTime();
+  });
+}
+
+export function sortByAToZ(leaderboards: FormattedLeaderboardData[]) {
+  return leaderboards.sort((a, b) => {
+    return a.gameName.localeCompare(b.gameName);
+  });
+}
+
+export function sortByZToA(leaderboards: FormattedLeaderboardData[]) {
+  return leaderboards.sort((a, b) => {
+    return b.gameName.localeCompare(a.gameName);
+  });
+}
