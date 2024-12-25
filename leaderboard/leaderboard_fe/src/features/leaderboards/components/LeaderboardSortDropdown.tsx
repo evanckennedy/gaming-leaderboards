3e63d4384c350778d49d26479410135fb2dae214
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-25 17:05:13 -0600

    Update LeaderboardSortDropdown.tsx for responsiveness

--- leaderboard/leaderboard_fe/src/features/leaderboards/components/LeaderboardSortDropdown.tsx
+++ leaderboard/leaderboard_fe/src/features/leaderboards/components/LeaderboardSortDropdown.tsx
@@ -30,18 +30,18 @@ function LeaderboardSortDropdown({
       <button
         type="button"
         onClick={toggleModal}
-        className="uppercase text-white-100 hover:text-secondary transition-colors duration-300 ease-out cursor-pointer"
+        className="uppercase text-white-100 3xl:text-2xl 4xl:text-5xl hover:text-secondary transition-colors duration-300 ease-out cursor-pointer"
       >
         {options.find((option) => option.value === value)?.label}
       </button>
 
       {isOpen && (
-        <div className="absolute top-full left-0 bg-primary-500 mt-2">
+        <div className="absolute top-full left-0 bg-primary-500 mt-2 3xl:mt-3 4xl:mt-6">
           {options.map((option) => (
             <button
               key={option.value}
               onClick={() => handleSelect(option.value)}
-              className="block w-full text-left text-white-100 hover:bg-primary-400 transition-colors duration-300 ease-out px-4 py-2"
+              className="block w-full text-left text-white-100 3xl:text-2xl 4xl:text-5xl hover:bg-primary-400 transition-colors duration-300 ease-out px-4 3xl:px-6 4xl:px-12 py-2 3xl:py-3 4xl:py-6"
             >
               {option.label}
             </button>
