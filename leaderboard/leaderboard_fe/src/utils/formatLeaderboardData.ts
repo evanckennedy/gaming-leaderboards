30ba7594daba3097efed4b1333e1f6ddf81d8a46
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-28 14:04:52 -0600

    Add leaderboard ID to formatted leaderboard data

--- leaderboard/leaderboard_fe/src/utils/formatLeaderboardData.ts
+++ leaderboard/leaderboard_fe/src/utils/formatLeaderboardData.ts
@@ -14,6 +14,7 @@ export function formatLeaderboardData(leaderboards: Leaderboard[]) {
     const rawDate = new Date(leaderboard.gameDate);
 
     return {
+      id: leaderboard.id,
       gameName: leaderboard.game.name,
       // Store the raw date for sorting
       rawGameDate: rawDate,
