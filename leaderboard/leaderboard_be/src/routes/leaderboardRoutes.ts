952903cd2aa891756084fc93cec035cf053e09eb
Evan Kennedy <evanckennedy@protonmail.com>
2024-12-29 17:31:57 -0600

    Update leaderboardRoutes.ts with DELETE route for leaderboard by sessionId

--- leaderboard/leaderboard_be/src/routes/leaderboardRoutes.ts
+++ leaderboard/leaderboard_be/src/routes/leaderboardRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createLeaderboard,
+  deleteLeaderboard,
   getLeaderboards,
 } from "../controllers/leaderboardController";
 import { verifyToken } from "../middleware/authMiddleware";
@@ -8,13 +9,23 @@ import { checkRole } from "../middleware/roleMiddleware";
 
 const router = Router();
 
-// Apply middleware only to the POST route
+// POST route for creating a leaderboard
 router.post(
   "/leaderboards",
   verifyToken,
   checkRole(["Root", "Create"]),
   createLeaderboard,
 );
+
+// GET route for fetching leaderboards
 router.get("/leaderboards", getLeaderboards);
+
+// DELETE route for deleting a leaderboard by sessionId
+router.get(
+  "/leaderboards/:id",
+  verifyToken,
+  checkRole(["Root"]),
+  deleteLeaderboard,
+);
 
 export default router;
